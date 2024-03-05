d01a0f068acb1e7b2ef63c504ed5a08b96a36761
Almir <almiruwow@gmail.com>
2024-03-05 13:34:21 +0300

    Реализация класса Channel

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,9 @@
+import json
+import os
+from googleapiclient.discovery import build
+
+api_key = os.getenv('YT_API_KEY')
+youtube = build('youtube', 'v3', developerKey=api_key)
 
 
 class Channel:
@@ -5,8 +11,12 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
 
     def print_info(self) -> None:
-        """Выводит в консоль информацию о канале."""
-        pass
+        data_chanel = youtube.channels().list(id=self.channel_id, part='snippet, statistics').execute()
+        print(json.dumps(data_chanel, indent=2, ensure_ascii=False))
+
+
+yt = Channel('UCwHL6WHUarjGfUM_586me8w')
+yt.print_info()
