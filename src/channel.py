7f21df1f4ec1d2ecef0c98c7ed62907e9019d0ac
Almir <almiruwow@gmail.com>
2024-03-06 21:15:45 +0300

    Добавление класса Channel

--- src/channel.py
+++ src/channel.py
@@ -1,6 +1,6 @@
-import json
 import os
 from googleapiclient.discovery import build
+import json
 
 api_key = os.getenv('YT_API_KEY')
 youtube = build('youtube', 'v3', developerKey=api_key)
@@ -11,9 +11,20 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        self._channel_id = channel_id
+        self.data_chanel = youtube.channels().list(id=self._channel_id, part='snippet, statistics').execute()
+        self.title = self.data_chanel["items"][0]["snippet"]['title']
+        self.video_count = self.data_chanel["items"][0]["statistics"]['videoCount']
+        self.url = 'https://www.youtube.com/channel/' + channel_id
 
-    def print_info(self) -> None:
-        data_chanel = youtube.channels().list(id=self.channel_id, part='snippet, statistics').execute()
-        print(json.dumps(data_chanel, indent=2, ensure_ascii=False))
+    @property
+    def channel_id(self):
+        return self._channel_id
 
+    @staticmethod
+    def get_service():
+        return youtube
+
+    def to_json(self, filename):
+        with open(f'{filename}', 'w', encoding='utf-8') as file:
+            json.dump(self.data_chanel, file, indent=2)
